ee8668de4b45a376ccbd2e44bf703d0c9d26bacb
LEAP\aamer.sadiq <aamer.sadiq@leapdev.io>
2017-10-25 11:27:59 +1100

    bagel file added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppComponent } from './layouts/app-root/app.component';
+import { AppComponent } from './layouts';
 
 @NgModule({
   declarations: [
