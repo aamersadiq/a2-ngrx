ada96f65f520c3f3d8b256149827da5581581b82
LEAP\aamer.sadiq <aamer.sadiq@leapdev.io>
2017-10-26 11:44:07 +1100

    actionLogger and storeFreeze added

--- src/app/reducers/index.ts
+++ src/app/reducers/index.ts
@@ -1,9 +1,6 @@
-
-
-
 import { compose } from '@ngrx/core/src/compose';
-
-// import { storeFreeze } from 'ngrx-store-freeze';
+import { storeLogger } from 'ngrx-store-logger';
+import { storeFreeze } from 'ngrx-store-freeze';
 import { combineReducers } from '@ngrx/store';
 
 import personReducer, * as fromPerson from '../person/reducers';
@@ -12,6 +9,6 @@ export interface AppState {
     person: fromPerson.PersonState;
 }
 
-export default compose(combineReducers)({//storeFreeze, storeLogger(),
+export default compose(storeFreeze, storeLogger(),combineReducers)({
     person: personReducer
 });
