651379c62bf94688cef13bc1f0ce37aed85bb669
LEAP\aamer.sadiq <aamer.sadiq@leapdev.io>
2017-11-10 11:01:40 +1100

    dev tool added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { StoreModule } from '@ngrx/store';
 import { HttpModule } from '@angular/http'
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 
 import { reducers, metaReducers } from './reducers';
 import { AppComponent } from './layouts';
@@ -10,6 +11,7 @@ import { routing } from './app.route'
 import { AuthorModule } from './author' 
 import { materialAnimationModules } from './shared/material/index'
 import { SharedModule } from './shared/shared.module'
+import { environment } from '@env/environment';
 
 @NgModule({
   declarations: [
@@ -23,6 +25,9 @@ import { SharedModule } from './shared/shared.module'
     SharedModule,
     AuthorModule,
     StoreModule.forRoot(reducers, { metaReducers }),
+    !environment.production ? StoreDevtoolsModule.instrument({
+      maxAge: 25 //  Retains last 25 states
+    }) : [ ]
   ],
   providers: [],
   bootstrap: [AppComponent]
